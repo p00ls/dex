bc369f98deb2873676d47d7cf928d322ca0b1bc7
Josselin Auguste <josselin@p00ls.io>
2022-09-15 16:09:23 +0200

    Remove last tokens

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -25,27 +25,6 @@ export const ZEROZERO: { [chainId: number]: Token } = {
     '00'
   ),
 }
-export const DAI = new Token(
-  SupportedChainId.MAINNET,
-  '0x6B175474E89094C44Da98b954EedeAC495271d0F',
-  18,
-  'DAI',
-  'Dai Stablecoin'
-)
-export const USDC = new Token(
-  SupportedChainId.MAINNET,
-  '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
-  6,
-  'USDC',
-  'USD//C'
-)
-export const WBTC = new Token(
-  SupportedChainId.MAINNET,
-  '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599',
-  8,
-  'WBTC',
-  'Wrapped BTC'
-)
 
 export const WETH9_EXTENDED: { [chainId: number]: Token } = {
   [SupportedChainId.MAINNET]: new Token(
