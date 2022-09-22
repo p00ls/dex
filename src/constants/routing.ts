9bc4b27267e1e25e6796ee231c6606e9e915744d
Josselin Auguste <josselin@p00ls.io>
2022-09-22 09:46:15 +0200

    Remove WETH from routing

--- src/constants/routing.ts
+++ src/constants/routing.ts
@@ -2,7 +2,7 @@
 import { Currency, Token } from '@uniswap/sdk-core'
 
 import { SupportedChainId } from './chains'
-import { WETH9_EXTENDED, ZEROZERO } from './tokens'
+import { ZEROZERO } from './tokens'
 
 type ChainTokenList = {
   readonly [chainId: number]: Token[]
@@ -12,12 +12,8 @@ type ChainCurrencyList = {
   readonly [chainId: number]: Currency[]
 }
 
-const WETH_ONLY: ChainTokenList = Object.fromEntries(
-  Object.entries(WETH9_EXTENDED).map(([key, value]) => [key, [value]])
-)
-
 // used to construct intermediary pairs for trading
-export const BASES_TO_CHECK_TRADES_AGAINST: ChainTokenList = WETH_ONLY
+export const BASES_TO_CHECK_TRADES_AGAINST: ChainTokenList = []
 export const ADDITIONAL_BASES: { [chainId: number]: { [tokenAddress: string]: Token[] } } = {}
 /**
  * Some tokens can only be swapped via certain pairs, so we override the list of bases that are considered for these
@@ -35,5 +31,5 @@ export const COMMON_BASES: ChainCurrencyList = {
 }
 
 // used to construct the list of all pairs we consider by default in the frontend
-export const BASES_TO_TRACK_LIQUIDITY_FOR: ChainTokenList = WETH_ONLY
+export const BASES_TO_TRACK_LIQUIDITY_FOR: ChainTokenList = []
 export const PINNED_PAIRS: { readonly [chainId: number]: [Token, Token][] } = {}
