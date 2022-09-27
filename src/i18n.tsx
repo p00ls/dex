2b075097c1e3e7d82050094c5ff68958670034c7
Josselin Auguste <josselin@p00ls.io>
2022-09-27 10:53:34 +0200

    Remove non-supported locales

--- src/i18n.tsx
+++ src/i18n.tsx
@@ -2,39 +2,7 @@ import { i18n } from '@lingui/core'
 import { I18nProvider } from '@lingui/react'
 import { DEFAULT_LOCALE, DEFAULT_MESSAGES, SupportedLocale } from 'constants/locales'
 import { initialLocale, useActiveLocale } from 'hooks/useActiveLocale'
-import {
-  af,
-  ar,
-  ca,
-  cs,
-  da,
-  de,
-  el,
-  en,
-  es,
-  fi,
-  fr,
-  he,
-  hu,
-  id,
-  it,
-  ja,
-  ko,
-  nl,
-  no,
-  pl,
-  PluralCategory,
-  pt,
-  ro,
-  ru,
-  sr,
-  sv,
-  sw,
-  tr,
-  uk,
-  vi,
-  zh,
-} from 'make-plural/plurals'
+import { en, es, fr, PluralCategory, pt } from 'make-plural/plurals'
 import { useEffect } from 'react'
 import { ReactNode } from 'react'
 import { useUserLocaleManager } from 'state/user/hooks'
@@ -44,38 +12,10 @@ type LocalePlural = {
 }
 
 const plurals: LocalePlural = {
-  'af-ZA': af,
-  'ar-SA': ar,
-  'ca-ES': ca,
-  'cs-CZ': cs,
-  'da-DK': da,
-  'de-DE': de,
-  'el-GR': el,
   'en-US': en,
   'es-ES': es,
-  'fi-FI': fi,
   'fr-FR': fr,
-  'he-IL': he,
-  'hu-HU': hu,
-  'id-ID': id,
-  'it-IT': it,
-  'ja-JP': ja,
-  'ko-KR': ko,
-  'nl-NL': nl,
-  'no-NO': no,
-  'pl-PL': pl,
   'pt-BR': pt,
-  'pt-PT': pt,
-  'ro-RO': ro,
-  'ru-RU': ru,
-  'sr-SP': sr,
-  'sv-SE': sv,
-  'sw-TZ': sw,
-  'tr-TR': tr,
-  'uk-UA': uk,
-  'vi-VN': vi,
-  'zh-CN': zh,
-  'zh-TW': zh,
 }
 
 async function dynamicActivate(locale: SupportedLocale) {
