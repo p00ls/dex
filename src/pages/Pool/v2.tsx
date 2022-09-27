cee1b7c62474e841188ee18cad6753c4fa63abfb
Josselin Auguste <josselin@p00ls.io>
2022-09-27 11:28:50 +0200

    Remove V2 from locales

--- src/pages/Pool/v2.tsx
+++ src/pages/Pool/v2.tsx
@@ -104,7 +104,7 @@ export default function Pool() {
               <TitleRow style={{ marginTop: '1rem' }} padding={'0'}>
                 <HideSmall>
                   <ThemedText.MediumHeader style={{ marginTop: '0.5rem', justifySelf: 'flex-start' }}>
-                    <Trans>Your V2 liquidity</Trans>
+                    <Trans>Your liquidity</Trans>
                   </ThemedText.MediumHeader>
                 </HideSmall>
                 <ButtonRow>
@@ -115,7 +115,7 @@ export default function Pool() {
                   </ResponsiveButtonPrimary>
                   <ResponsiveButtonPrimary id="join-pool-button" as={Link} to="/add/v2/00" padding="6px 8px">
                     <Text fontWeight={500} fontSize={16}>
-                      <Trans>Add V2 Liquidity</Trans>
+                      <Trans>Add Liquidity</Trans>
                     </Text>
                   </ResponsiveButtonPrimary>
                 </ButtonRow>
